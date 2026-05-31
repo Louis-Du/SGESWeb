78f5b42992749a22ec09f61f47c1a85b19001c55
Louis-Du <L.a.d.f.beto@gmail.com>
2026-05-30 22:46:08 -0600

    feat: prueba de interfaz crear evento

---
+++ SGES.Web/SGES.Web/Controllers/EventoController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SGESWeb.Controllers
+{
+    public class EventoController : Controller
+    {
+        public ActionResult CrearEvento()
+        {
+            return View();
+        }
+    }
+}
