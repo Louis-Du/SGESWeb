b019264d38152bd9c0b9799568350e6708bfd157
lukasa133 <ldiazc3@soy.sena.edu.co>
2026-05-31 15:24:51 -0500

    feat: EventsController

---
+++ SGES.Web/SGES.Web/Controllers/EventsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SGES.Web.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly IEventService _eventService;
+
+        public EventsController(IEventService eventService)
+        {
+            _eventService = eventService;
+        }
+
+        public ActionResult Index()
+        {
+            var eventos = _eventService.GetAvailableEvents();
+
+            return View(eventos);
+        }
+    }
+}
