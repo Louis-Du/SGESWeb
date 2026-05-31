e4eb4dc3081b1242009eea5b1e3922c1ba5ab781
lukasa133 <ldiazc3@soy.sena.edu.co>
2026-05-31 18:59:59 -0500

    delete: eventscontroller.cs

---
+++ SGES.Web/SGES.Web/Models/EventoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SGESWeb.Models
+{
+    public class EventoModel
+    {
+        public int IdEvento { get; set; }
+
+        public string NombreEvento { get; set; }
+
+        public string TipoEvento { get; set; }
+
+        public DateTime DiaEvento { get; set; }
+
+        public DateTime FechaHoraInicio { get; set; }
+
+        public DateTime FechaHoraFin { get; set; }
+
+        public int IdUser { get; set; }
+    }
+}
