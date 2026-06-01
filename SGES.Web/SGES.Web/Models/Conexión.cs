abb5fa087ce56cf692ba8f751de8b4521d838b9d
lukasa133 <ldiazc3@soy.sena.edu.co>
2026-05-31 19:01:40 -0500

    feat: conexion y crearevento
    agrego los modelos correspondientes de la rama de mi HU-9-CREAR-EVENTOS

---
+++ SGES.Web/SGES.Web/Models/Conexión.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace SGESWeb.Models
+{
+    public class Conexion
+    {
+        private readonly string cadenaConexion;
+
+        public Conexion()
+        {
+            cadenaConexion = ConfigurationManager
+                .ConnectionStrings["SGESConnection"]
+                .ConnectionString;
+        }
+
+        public SqlConnection ObtenerConexion()
+        {
+            return new SqlConnection(cadenaConexion);
+        }
+    }
+}
---
+++ SGES.Web/SGES.Web/Models/EventoDAO.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace SGESWeb.Models
+{
+    public class EventoDAO
+    {
+        private readonly Conexion cn = new Conexion();
+
+        public List<EventoModel> ObtenerEventos()
+        {
+            List<EventoModel> lista = new List<EventoModel>();
+
+            using (SqlConnection con = cn.ObtenerConexion())
+            {
+                string sql = "SELECT * FROM Eventos";
+
+                SqlCommand cmd = new SqlCommand(sql, con);
+
+                con.Open();
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    lista.Add(new EventoModel
+                    {
+                        IdEvento = Convert.ToInt32(dr["idEvento"]),
+                        NombreEvento = dr["nombreEvento"].ToString(),
+                        TipoEvento = dr["tipoEvento"].ToString(),
+                        FechaHoraInicio = Convert.ToDateTime(dr["fechaHoraInicio"]),
+                        FechaHoraFin = Convert.ToDateTime(dr["fechaHoraFin"]),
+                        IdUser = Convert.ToInt32(dr["idUser"])
+                    });
+                }
+            }
+
+            return lista;
+        }
+
+        public void InsertarEvento(EventoModel evento)
+        {
+            using (SqlConnection con = cn.ObtenerConexion())
+            {
+                string query = @"INSERT INTO Eventos (nombreEvento, tipoEvento, diaEvento, fechaHoraInicio, fechaHoraFin, idUser)
+                               VALUES (@nombreEvento, @tipoEvento, @diaEvento, @fechaHoraInicio, @fechaHoraFin, @idUser)";
+
+                SqlCommand cmd = new SqlCommand(query, con);
+
+                cmd.Parameters.AddWithValue("@idEvento", evento.IdEvento);
+                cmd.Parameters.AddWithValue("@nombreEvento", evento.NombreEvento);
+                cmd.Parameters.AddWithValue("@tipoEvento", evento.TipoEvento);
+                cmd.Parameters.AddWithValue("@diaEvento", evento.FechaHoraInicio.Date);
+                cmd.Parameters.AddWithValue("@fechaHoraInicio", evento.FechaHoraInicio);
+                cmd.Parameters.AddWithValue("@fechaHoraFin", evento.FechaHoraFin);
+                cmd.Parameters.AddWithValue("@idUser", evento.IdUser);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
