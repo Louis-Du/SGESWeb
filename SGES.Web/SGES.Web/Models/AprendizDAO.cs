614291ab94d32fadfa3be9d1956109404d69a767
lukasa133 <ldiazc3@soy.sena.edu.co>
2026-06-05 10:36:44 -0500

    feat: Models para Aprendices
    agrego las clases para modelo para el llamado en la base de datos

---
+++ SGES.Web/SGES.Web/Models/AprendizDAO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Web;
+
+namespace SGES.Web.Models
+{
+    public class AprendizDAO
+    {
+        private readonly Conexion cn = new Conexion();
+
+        public List<AprendizModel> ObtenerAprendices()
+        {
+            List<AprendizModel> lista = new List<AprendizModel>();
+
+            using (SqlConnection con = cn.ObtenerConexion())
+            {
+                string sql = "SELECT * FROM Aprendiz";
+
+                SqlCommand cmd = new SqlCommand(sql, con);
+
+                con.Open();
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    lista.Add(new AprendizModel
+                    {
+                        idApr = Convert.ToInt32(dr["idApr"]),
+                        nombreApr = dr["nombreApr"].ToString(),
+                        EmailApr = dr["emailApr"].ToString(),
+                        ContactoApr = dr["contactoApr"].ToString()
+                    });
+                }
+            }
+
+            return lista;
+        }
+    }
+}
---
+++ SGES.Web/SGES.Web/Models/AprendizModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SGES.Web.Models
+{
+    public class AprendizModel
+    {
+        public int idApr { get; set; }
+
+        public string nombreApr { get; set; }
+
+        public string EmailApr { get; set; }
+
+        public string ContactoApr { get; set; }
+    }
+}
