f57d4fcc9399114b7c3f60608914d6bda248f9d0
lukasa133 <ldiazc3@soy.sena.edu.co>
2026-06-07 22:53:57 -0500

    refactor: quitado de aliases AprendizModel
    quitado los aliases que correspondían para los atributos de clase AprendizModel, para mejor uso del controlador.

--- SGES.Web/SGES.Web/Models/AprendizModel.cs
+++ SGES.Web/SGES.Web/Models/AprendizModel.cs
@@ -7,24 +7,8 @@ namespace SGES.Web.Models
     {
         public int IdApr { get; set; }
         public string NombreApr { get; set; }
-
-        // Campo principal para contacto/email (llenado por DAO)
-        public string Email { get; set; }
-
-        // Alias por compatibilidad: algunas vistas/DAOs usaban ContactoApr o EmailApr
-        public string ContactoApr
-        {
-            get => Email;
-            set => Email = value;
-        }
-
-        // Alias adicional por compatibilidad si existe código que esperaba EmailApr
-        public string EmailApr
-        {
-            get => Email;
-            set => Email = value;
-        }
-            
-        public string Ficha { get; set; } // opcional
+        public string EmailApr { get; set; }     // campo real independiente
+        public string ContactoApr { get; set; }  // campo real independiente
+        public string Ficha { get; set; }
     }
 }
